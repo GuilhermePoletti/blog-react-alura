dbbc62b7ef0bc5ecbb255ecac87a980af9127322
Guilherme Poletti <gui.poletti@gmail.com>
2023-06-09 17:59:37 -0300

    pg sobre mim

--- src/componentes/Post/index.js
+++ src/componentes/Post/index.js
@@ -5,9 +5,9 @@ export default function Post ({ post }) {
         <div className={styles.post}>
             <img
                 className={styles.capa}
-                src={`assets/posts/${post.id}/capa.png`}
-                alt="Imagem de capa do Post"
-            />
+                src={`/assets/posts/${post.id}/capa.png`} 
+                alt=""
+            />           
             <h2 className={styles.titulo}> {post.titulo} </h2>
             <button className={styles.botaoLer}> Ler </button>
         </div>
--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,8 +1,21 @@
+import PostModelo from "componentes/PostModelo";
 
 export default function SobreMim() {
     return (
-        <main>
-            
-        </main>
+        <PostModelo
+            fotoCapa={fotoCapa}
+            titulo="Sobre mim"
+        >
+            <h3 className={style.subtitulo}>
+                Olá, eu sou o Guilherme!
+            </h3>
+
+            <img 
+                src={fotoSobreMim}
+                alt="Foto do Guilherme"
+                className={style.fotoSobreMim}
+            />
+
+        </PostModelo>               
     )
 }
